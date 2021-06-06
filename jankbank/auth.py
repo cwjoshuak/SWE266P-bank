4b77a6cb90a1d8a7feee6d01d3445a32137dc699
kthirano <kthirano@uci.edu>
2021-06-06 11:09:41 -0700

    SQL injection fix

--- jankbank/auth.py
+++ jankbank/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from jankbank.db import get_db
+from flaskr.db import get_db
 
 import re
 import sys
